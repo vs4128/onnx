013b705fea6dc55fa0bcae90f925cf941eec0c01
Venura Shyamal <venura.shyamal@zebra.com>
2025-09-16 15:55:34 +0530

    convert depthMap to pointCloud

--- app/src/main/cpp/onnx.cpp
+++ app/src/main/cpp/onnx.cpp
@@ -4,12 +4,17 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
+#include <thread>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
 
 #define LOG_TAG "onnxNative"
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
+
+cv::Mat depthMapToPointCloud(cv::Mat mat);
 
 extern "C"
 JNIEXPORT jobjectArray JNICALL
@@ -48,15 +53,45 @@ Java_com_example_onnx_MainActivity_nativeProcess(JNIEnv *env, jobject thiz,jobje
     int resizedWidth = 640;
     cv::resize(depth, resizedDepth, cv::Size(resizedWidth, resizedHeight), 0, 0, cv::INTER_LINEAR);
 
+    //convert depth map to point cloud
+    cv::Mat pointCloud = depthMapToPointCloud(resizedDepth);
+    //save this point cloud to
 
-    //convert depth to java array and return it to java
-    jobjectArray depthArray = env->NewObjectArray(resizedDepth.rows, env->FindClass("[F"), nullptr);
-    for (int i = 0; i < resizedDepth.rows; i++) {
-        jfloatArray row = env->NewFloatArray(resizedDepth.cols);
-        env->SetFloatArrayRegion(row, 0, resizedDepth.cols, resizedDepth.ptr<float>(i));
-        env->SetObjectArrayElement(depthArray, i, row);
-        env->DeleteLocalRef(row);
+
+//    auto thread = std::thread([this, path, image]() mutable
+//                              {
+//                                  pcl::io::savePCDFile(path, *image->getPointCloud());
+//                              });
+//    thread.detach();
+
+
+    return 1;
+}
+
+cv::Mat depthMapToPointCloud(cv::Mat depthMap) {
+    int pointCloudWidth = depthMap.cols;
+    int pointCloudHeigth = depthMap.rows;
+
+    float fx = 488.3022098;
+    float fy = 488.3022098;
+    float ppx = 320.99165;
+    float ppy = 235.4008464;
+    float depthScalar = 0.001;
+
+    //create a mat with 3 channels to hold the point cloud with sizes of pointCloudWidth and pointCloudHeigth
+    cv::Mat pointCloud(pointCloudHeigth, pointCloudWidth, CV_32FC3);
+
+    //fill the point cloud mat with the depth map values
+    for (int v = 0; v < pointCloudHeigth; ++v)
+    {
+        for (int u = 0; u < pointCloudWidth; ++u)
+        {
+            float z = static_cast<float>(depthMap.at<ushort>(v, u) * depthScalar);
+            float x = (static_cast<float>(u) - ppx) * z / fx;
+            float y = (static_cast<float>(v) - ppy) * z / fx;
+            pointCloud.at<cv::Vec3f>(v, u) = cv::Vec3f(x, y, z);
+        }
     }
 
-    return depthArray;
+    return pointCloud;
 }
